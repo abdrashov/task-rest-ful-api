f92cb9ce0a491903cd8ad6312d7ff4998e40f655
Abdrashov <abdrashovv@gmail.com>
2022-01-03 15:38:03 +0600

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -16,8 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     // test text
     return view('welcome');
+<<<<<<< HEAD
     // hello world
 
     // hhh
     // asds
+=======
+>>>>>>> parent of 4885613 (Update web.php)
 });
